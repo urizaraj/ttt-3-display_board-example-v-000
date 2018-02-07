cb8781ad6304fbd40aa7c711bfe117c96684c01c
Alexander Urizar <urizaraj@gmail.com>
2018-02-07 20:51:12 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board()
+  [0,3,6].each do |i|
+    puts "   |   |   "
+    if i != 6
+      puts '-----------'
+    end
+  end
+end
