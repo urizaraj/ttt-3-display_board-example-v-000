2f00885aa9ef20f3a5a174868d701d94e50a14ca
Alexander Urizar <urizaraj@gmail.com>
2018-02-07 20:50:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board(board)
   [0,3,6].each do |i|
-    # puts " #{board[i]} | #{board[i + 1]} | #{board[i + 2]} "
     puts "   |   |   "
     if i != 6
       puts '-----------'
     end
   end
 end
-  
